d0402e1a0aabd9b4521deae77e5f4f7964ecd173
Mohammad Assaf <mhamad234assaf@gmail.com>
2025-09-05 14:19:16 +0300

    State tutorial

--- composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -4,43 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import learn_app.composeapp.generated.resources.Res
-import learn_app.composeapp.generated.resources.lexend_bold
-import learn_app.composeapp.generated.resources.lexend_extra_bold
-import learn_app.composeapp.generated.resources.lexend_extra_light
-import learn_app.composeapp.generated.resources.lexend_light
-import learn_app.composeapp.generated.resources.lexend_medium
-import learn_app.composeapp.generated.resources.lexend_regular
-import learn_app.composeapp.generated.resources.lexend_semi_bold
-import learn_app.composeapp.generated.resources.lexend_thin
-import org.jetbrains.compose.resources.Font
+import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -49,101 +23,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val fontFamily = FontFamily(
-                Font( Res.font.lexend_bold, weight = FontWeight.Bold),
-                Font(Res.font.lexend_extra_bold, weight = FontWeight.ExtraBold),
-                Font(Res.font.lexend_extra_light, weight = FontWeight.ExtraLight),
-                Font(Res.font.lexend_light, weight = FontWeight.Light),
-                Font(Res.font.lexend_medium, weight = FontWeight.Medium),
-                Font(Res.font.lexend_regular, weight = FontWeight.Normal),
-                Font(Res.font.lexend_semi_bold, weight = FontWeight.SemiBold),
-                Font(Res.font.lexend_thin, weight = FontWeight.Thin),
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 40.dp)
-                    .background(Color(0xFF101010))
+            val color = remember {
+                mutableStateOf(Color.Yellow)
+            }
+            Column(
+                modifier = Modifier.fillMaxSize()
             ) {
-                Text(
-                    text= buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ){
-                            append("H")
-                        }
-                        append("ello ")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ){
-                            append("W")
-                        }
-                        append("orld!")
-                    },
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Bold,
-                    fontStyle = FontStyle.Italic,
-                    textAlign = TextAlign.Center,
+                ColorBox(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) { color1: Color, string: String ->
+                    color.value = color1
+                    println(string)
+                }
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .background(color.value)
                 )
             }
         }
     }
 }
 
-
 @Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
+fun ColorBox(
+    modifier: Modifier = Modifier,
+    updateColor: (color: Color, name: String) -> Unit
 ) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = CardDefaults.cardElevation(disabledElevation = 5.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .fillMaxSize()
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
-                    )
-            )
-            Text(
-                title,
-                style = TextStyle(color = Color.White, fontSize = 16.sp),
-                modifier = Modifier
-                    .align(alignment = Alignment.BottomStart)
-                    .padding(12.dp)
-            )
-        }
 
-    }
-
+    Box(
+        modifier = modifier
+            .background(Color.Red)
+            .clickable {
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
+                    ), "Hello"
+                )
+            }
+    ) {}
 }
